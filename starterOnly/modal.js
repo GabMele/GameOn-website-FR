a40f9c1160957b07bd9f7cf282a4a1bb0b023e9a
Gabriele Melendugno <77445560+GabMele@users.noreply.github.com>
2024-03-05 17:40:51 +0100

    feat : issue TODO : fermer la modale #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -5,12 +5,17 @@ function editNav() {
   } else {
     x.className = "topnav";
   }
+
+  // testgab
+  console.log(x.className);
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +23,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  
+  
+  // closing modal event
+  closeBtn.addEventListener("click", () => {
+    modalbg.style.display = "none"
+  });
+
+
+
 }
 
 
